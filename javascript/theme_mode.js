59ccf621d0eb611a642364b9f61fb70e193c44bc
Tatsuya28 <ioiodauphin880@gmail.com>
2023-04-25 15:22:19 +0200

    dark mode accueil

--- javascript/theme_mode.js
+++ javascript/theme_mode.js
@@ -4,12 +4,16 @@ function changeMode() {
     if(toggleTheme == 1) {
 
         document.documentElement.style.setProperty('--ecriture', '#262626');
+        document.documentElement.style.setProperty('--default-text', '#000000');
+        document.documentElement.style.setProperty('--ligneSection', '#352f2f8a');
         document.documentElement.style.setProperty('--color1', '#fcfaff');
         toggleTheme = 0;
 
     } else {
 
         document.documentElement.style.setProperty('--ecriture', '#fcfaff');
+        document.documentElement.style.setProperty('--default-text', '#fff');
+        document.documentElement.style.setProperty('--ligneSection', '#fcfaff');
         document.documentElement.style.setProperty('--color1', '#262626');
         toggleTheme = 1;
 
